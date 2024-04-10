f61d954d2824a73ce079bbf99434eb87bf1b6056
Jason Lee <jasonlee@github.com>
2024-04-10 13:24:15 -0700

    Updated noteCreate controller to handle POST request from creating a new note with an INSERT statement into the notes table; added htmlspecialchars() calls to where the notes are displayed (My Notes and Notes individual page) to sanitize user input a bit more

--- demo/controllers/noteCreate.php
+++ demo/controllers/noteCreate.php
@@ -1,9 +1,26 @@
 <?php
 
+namespace demo\controllers;
+
+use Database;
+
+$config = require('config.php');
+$db = new Database(
+    config: $config['database'],
+    password: $config['database']['password']
+);
+
 $heading = 'Create Note';
 
+/**
+ * Considering how a user can affect what can be included in the POST request, we need to find 
+ * a way to sanitize user input before it's stored and displayed anywhere on our page.
+ */
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :userId)', [
+        'body' => $_POST['body'],
+        'userId' => 1, // TODO: This needs to be the userId of the current user submitting the request
+    ]);
 }
 
 require 'views/noteCreate.view.php';
--- demo/views/notes.view.php
+++ demo/views/notes.view.php
@@ -8,13 +8,17 @@
             <?php foreach ($notes as $note) : ?>
                 <li>
                     <a href="/note?id=<?= $note['id'] ?>" class=" text-blue-500 hover:underline">
-                        <?= $note['body'] ?>
+                        <!-- 
+                            htmlspecialchars() is a built-in PHP function that converts special chars to HTML entities.
+                            This helps prevent malicious user input that takes the form of HTML to cause unintended behaviors.
+                        -->
+                        <?= htmlspecialchars($note['body']) ?>
                     </a>
                 </li>
             <?php endforeach; ?>
         </ul>
 
-        <!-- margin top = 6 -->
+        <!-- mt-6 === margin top = 6 -->
         <p class="mt-6">
             <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
         </p>
